2b7cad1e5bd7fbe8d2a8b3c6488c8800fc494893
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2022-09-23 03:43:14 +0000

    simulacion fundamentacion 3

--- SL-GCN/runModel.sh
+++ SL-GCN/runModel.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
-
 #python main.py --config config/sign/train/train_joint.yaml
   #num_class: 28 # AEC=28, PUCP=29 , WLASL=86
-
   #num_point: 29 # 29 or 71
-
 
 declare -a points=(29 71 29 71 29 71)
 declare -a lrs=(0.05 0.1 0.05 0.1 0.1 0.05)
@@ -14,12 +10,12 @@ declare -a datasets=("AEC" "AEC" "PUCP" "PUCP" "WLASL" "WLASL")
 
 
 
-for i in 5 15 25 35 45 
+for i in 0 1 5 15 25 35 45 55 65 75 85 95
 do
   for j in 0 1 2 3 4 5
   do 
-      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[J]}/wholepose-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model wholepose --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 400  --mode_train fundamentacion
-      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[J]}/mediapipe-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model mediapipe --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 400  --mode_train fundamentacion --weights "save_models/results/${points[j]}/${datasets[j]}/wholepose-AEC-s-$i/wholepose-${datasets[j]}-${points[j]}-$i-init.pt"
-      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[J]}/openpose-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model openpose --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 400  --mode_train fundamentacion --weights "save_models/results/${points[j]}/${datasets[j]}/wholepose-AEC-s-$i/wholepose-${datasets[j]}-${points[j]}-$i-init.pt"
+      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[j]}/wholepose-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model wholepose --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 500  --mode_train fundamentacion_3
+      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[j]}/mediapipe-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model mediapipe --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 500  --mode_train fundamentacion_3 --weights "save_models/results/${points[j]}/${datasets[j]}/wholepose-${datasets[j]}-s-$i/wholepose-${datasets[j]}-${points[j]}-$i-init.pt"
+      python main.py --seed $i --experiment_name "results/${points[j]}/${datasets[j]}/openpose-${datasets[j]}-s-$i" --database ${datasets[j]} --keypoints_model openpose --base_lr ${lrs[j]} --keypoints_number ${points[j]} --num_epoch 500  --mode_train fundamentacion_3 --weights "save_models/results/${points[j]}/${datasets[j]}/wholepose-${datasets[j]}-s-$i/wholepose-${datasets[j]}-${points[j]}-$i-init.pt"
   done
 done
